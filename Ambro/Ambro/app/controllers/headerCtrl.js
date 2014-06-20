75a8923db112e7bf364fcd29e6faa61fde4ae904
Kyle Curren <ksc4130@gmail.com>
2014-06-20 18:41:27 -0400

    check check

---
+++ Ambro/Ambro/app/controllers/headerCtrl.js
@@ -0,0 +1,13 @@
+﻿(function() {
+    'use strict';
+
+    headerCtrl.$inject = ['$scope', '$location'];
+    ambro.controller('headerCtrl', headerCtrl);
+    function headerCtrl($scope, $location) {
+        $scope.active = function (url) {
+            var p = $location.path();
+            console.log(p);
+            return p.indexOf(url) > -1;
+        }
+    }
+}());
