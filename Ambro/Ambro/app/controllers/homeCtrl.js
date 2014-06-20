e4c3192ce9c43548df0ebab03ea42c49fca69442
Kyle Curren <ksc4130@gmail.com>
2014-06-19 21:49:52 -0400

    pastries and cookies

--- Ambro/Ambro/app/controllers/homeCtrl.js
+++ Ambro/Ambro/app/controllers/homeCtrl.js
@@ -5,6 +5,12 @@
     ambro.controller('homeCtrl', homeCtrl);
     function homeCtrl($scope) {
         $scope.myInterval = 5000;
+
+        $scope.imgs = [
+            { title: 'One', imgUrl: 'http://owlgraphic.com/owlcarousel/demos/assets/fullimage1.jpg' },
+            { title: 'Two', imgUrl: 'http://owlgraphic.com/owlcarousel/demos/assets/fullimage2.jpg' }
+        ];
+
         var slides = $scope.slides = [];
         $scope.addSlide = function () {
             var newWidth = 600 + slides.length;
--- Ambro/Ambro/app/directives.js
+++ Ambro/Ambro/app/directives.js
@@ -1,4 +1,4 @@
-﻿(function() {
+﻿(function () {
     'use strict';
 
     ambro.directive('dots', dots);
@@ -14,6 +14,47 @@
             link: function (scope, el, attrs) {
                 scope.show = scope.value && (!scope.limit || scope.value.length > scope.limit);
             }
+        }
+
     }
+
+    slider.$inject = ['$interval'];
+    ambro.directive('slider', slider);
+    function slider($interval) {
+        return {
+            replace: true,
+            restrict: 'E',
+            templateUrl: '/app/tmpls/sliderDrc.html',
+            scope: {
+                imgs: '=',
+                interval: '@'
+            },
+            link: function (scope, el, attrs) {
+                scope.imgs.forEach(function(item) {
+                    item.sliderClass = 'am-fade';//'am-slide-right';
+                });
+
+                scope.disImgs = [];
+                scope.disImgs.push(scope.imgs[0]);
+                //scope.disImgs[0].sliderClass = 'am-slide-left';
+                scope.interval = scope.interval || 1500;
+
+                var curIndex = 0;
+                var stop = $interval(function () {
+                    curIndex += 1;
+
+                    if (curIndex > (scope.imgs.length - 1)) {
+                        curIndex = 0;
+                    }
+
+                    console.log('cur index', curIndex, scope.imgs[curIndex].sliderClass);
+
+                   // scope.disImgs[0].sliderClass = 'am-slide-right';
+                    scope.disImgs = [];
+                    scope.disImgs.push(scope.imgs[curIndex]);
+                    //scope.disImgs[0].sliderClass = 'am-slide-left';
+                }, scope.interval);
+            }
+        }
     }
 }());
