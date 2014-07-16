f1bef222a90bbd794a5c1430529be6a1f3493a3d
Kyle Curren <ksc4130@gmail.com>
2014-07-15 20:04:38 -0400

    update

--- Ambro/Ambro/app/controllers/homeCtrl.js
+++ Ambro/Ambro/app/controllers/homeCtrl.js
@@ -1,24 +1,107 @@
-﻿(function() {
+﻿(function () {
     'use strict';
 
-    homeCtrl.$inject = ['$scope', 'packages'];
+    homeCtrl.$inject = ['$scope', 'packages', '$interval'];
     ambro.controller('homeCtrl', homeCtrl);
-    function homeCtrl($scope, packages) {
-        $scope.imgs = packages.map(function(item) {
+    function homeCtrl($scope, packages, $interval) {
+        $scope.imgs = packages.map(function (item) { //.splice(0, 5)
             return {
                 title: item.product.productName,
                 imgUrl: item.imgUrl,
-                href: '#/product/' + item.id
+                href: '#/product/' + item.id,
+                isActive: false,
+                transStyle: {},
+                getTransStyle: function () {
+                    var i = $scope.imgs.indexOf(this),
+                        calc = (i * -500);
+                    return {
+                        transform: 'translateX(' + (i === 0 ? 0 : calc) + 'px)'
+                    }
+                },
+                style: {
+                    left: '500px'
+                }
                 //, description: item.product.productDescription
             };
         });
 
-//            [
-//            { title: 'Baklava', imgUrl: '/Content/img/slider/img_0896.jpg', description: '' },
-//            { title: 'Melomakarona', imgUrl: '/Content/img/slider/img_0942.jpg', description: '' },
-//            { title: 'Paximadakia', imgUrl: '/Content/img/slider/img_0950.jpg', description: '' },
-//            { title: 'Kourambiedes', imgUrl: '/Content/img/slider/img_0907.jpg', description: '' },
-//            { title: 'Assorted Box', imgUrl: '/Content/img/slider/img_0920two.jpg', description: '' }
-//        ];
+        $scope.slideImgs = [packages[0]];
+
+        var transWidth = $scope.imgs.length * 500;
+        $scope.transStyle = {
+            width: transWidth + 'px'
+        };
+
+        var last = null,
+            current = null,
+            curIndex = 0,
+            total = $scope.imgs.length,
+            slideImgsFull = false;
+
+        $scope.imgOne = { style: { left: '500px' } };
+
+        $scope.imgTwo = {
+            style: {left: '0px'},
+            imgUrl: $scope.imgs[curIndex].imgUrl
+        };
+        $scope.imgThree = {
+            style: { left: '500px' },
+            imgUrl: $scope.imgs[curIndex + 1].imgUrl
+        };
+
+        var prev = $scope.imgOne,
+            cur = $scope.imgTwo,
+            next = $scope.imgThree;
+
+        $interval(function () {
+
+            if (last) {
+                last.isActive = false;
+                // last.transStyle = {};
+            }
+
+            curIndex += 1;
+            if (curIndex >= total) {
+                curIndex = 0;
+                slideImgsFull = true;
+
+            } else if (!slideImgsFull) {
+                $scope.slideImgs.push($scope.imgs[curIndex]);
+            }
+
+            last = current;
+            current = $scope.imgs[curIndex];
+            current.active = true;
+            //current.transStyle = current.getTransStyle();
+            //$scope.transStyle = current.getTransStyle();
+            $scope.transStyle = {
+                transform: 'translateX(' + (curIndex * -500) + 'px)',
+                width: transWidth + 'px'
+            };
+            console.log('change', current.getTransStyle());
+
+            prev.imgUrl = curIndex === total ? $scope.imgs[0].imgUrl : $scope.imgs[curIndex + 1].imgUrl;
+
+            prev.style.opacity = 0;
+            prev.style.left = '500px';
+
+            next.style.opacity = 1;
+            cur.style.left = '-500px';
+            next.style.left = '0px';
+
+            var lastPrev = prev;
+            prev = cur;
+            cur = next;
+            next = lastPrev;
+        }, 3000);
+
+
+        //            [
+        //            { title: 'Baklava', imgUrl: '/Content/img/slider/img_0896.jpg', description: '' },
+        //            { title: 'Melomakarona', imgUrl: '/Content/img/slider/img_0942.jpg', description: '' },
+        //            { title: 'Paximadakia', imgUrl: '/Content/img/slider/img_0950.jpg', description: '' },
+        //            { title: 'Kourambiedes', imgUrl: '/Content/img/slider/img_0907.jpg', description: '' },
+        //            { title: 'Assorted Box', imgUrl: '/Content/img/slider/img_0920two.jpg', description: '' }
+        //        ];
     }
 }());
--- Ambro/Ambro/app/directives.js
+++ Ambro/Ambro/app/directives.js
@@ -18,6 +18,12 @@
 
     }
 
+    changer.$inject = [];
+    ambro.directive('changer', changer);
+    function changer() {
+        
+    }
+
     slider.$inject = ['$interval'];
     ambro.directive('slider', slider);
     function slider($interval) {
@@ -30,37 +36,55 @@
                 interval: '@',
                 defaultClass: '@',
                 inClass: '@',
-                outClass: '@'
+                outClass: '@',
+                title: '=?',
+                description: '=?',
+                round: '=?',
+                thumbnailClass: '=?'
             },
-            link: function (scope, el, attrs) {
-                var defaultClass = scope.defaultClass || 'am-fade',
-                    inClass = scope.inClass || defaultClass,
-                    outClass = scope.outClass || defaultClass;
+            controller: ['$scope', function ($scope) {
+                var defaultClass = $scope.defaultClass || 'am-fade',
+                    inClass = $scope.inClass || defaultClass,
+                    outClass = $scope.outClass || defaultClass;
 
-                scope.imgs.forEach(function(item) {
-                    item.sliderClass = scope.inClass;//'am-fade';//'am-slide-right';
+                console.log('thumb', $scope.thumbnailClass);
+
+                if (typeof $scope.title !== 'boolean') {
+                    $scope.title = true;
+                }
+
+                if (typeof $scope.description !== 'boolean') {
+                    $scope.description = true;
+                }
+
+                if (typeof $scope.round !== 'boolean') {
+                    $scope.round = true;
+                }
+
+                $scope.imgs.forEach(function(item) {
+                    item.sliderClass = $scope.inClass;//'am-fade';//'am-slide-right';
                 });
 
-                scope.disImgs = [];
-                scope.disImgs.push(scope.imgs[0]);
-                scope.disImgs[0].sliderClass = outClass;
-                scope.interval = scope.interval || 1500;
+                $scope.disImgs = [];
+                $scope.disImgs.push($scope.imgs[0]);
+                $scope.disImgs[0].sliderClass = outClass;
+                $scope.interval = $scope.interval || 1500;
 
                 var curIndex = 0;
                 var stop = $interval(function () {
                     curIndex += 1;
 
-                    if (curIndex > (scope.imgs.length - 1)) {
+                    if (curIndex > ($scope.imgs.length - 1)) {
                         curIndex = 0;
                     }
 
-                    var out = scope.disImgs[0];
-                    scope.disImgs = [];
-                    scope.disImgs.push(scope.imgs[curIndex]);
-                    scope.disImgs[0].sliderClass = outClass;
+                    var out = $scope.disImgs[0];
+                    $scope.disImgs = [];
+                    $scope.disImgs.push($scope.imgs[curIndex]);
+                    $scope.disImgs[0].sliderClass = outClass;
                     out.sliderClass = inClass;
-                }, scope.interval);
-            }
+                }, $scope.interval);
+            }]
         }
     }
 }());
