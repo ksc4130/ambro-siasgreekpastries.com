e2f8a93ec30b371d38612a8554d1f3c7cbbd391a
Kyle Curren <ksc4130@gmail.com>
2014-09-03 23:11:15 -0400

    bunch of fun stuff like favorsCtrl and links on splash iter fix for inter cancel on destory .... and more

---
+++ Ambro/Ambro/app/controllers/favorsCtrl.js
@@ -0,0 +1,23 @@
+﻿(function() {
+    'use strict';
+
+    favorsCtrl.$inject = ['$scope', 'utils'];
+    ambro.controller('favorsCtrl', favorsCtrl);
+    function favorsCtrl($scope, utils) {
+        var favorImgs = [
+            { imgUrl: '/Content/img/favors/Baby Shower Favor3-Piece Paximadia.jpg' },
+            { imgUrl: '/Content/img/favors/Chcolate dipped Paximadia2.jpg' },
+            //{ imgUrl: '/Content/img/favors/Chocolate dipped Paximadia.jpg' },
+            //{ imgUrl: '/Content/img/favors/Chocolate dipped Paximadia3.jpg' },
+            { imgUrl: '/Content/img/favors/Graduatin favor front only2.jpg' },
+            { imgUrl: '/Content/img/favors/Graduation favor front only.jpg' },
+            //{ imgUrl: '/Content/img/favors/Graduation favor showing chocolate covered baklava.jpg' },
+            { imgUrl: '/Content/img/favors/Graduation favor with Kourambie.jpg' },
+            { imgUrl: '/Content/img/favors/IMG_3770.jpg' },
+            { imgUrl: '/Content/img/favors/WeddingAnniversaryfavor chocolate-covered baklava.jpg' },
+            { imgUrl: '/Content/img/favors/White chocolat dipped paximadia.jpg' }
+        ];
+        var iter = utils.iter(favorImgs, $scope, 2000);
+        $scope.img = iter.value;
+    }
+}());
